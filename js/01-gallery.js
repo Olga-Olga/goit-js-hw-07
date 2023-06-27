fd652fdbf651c5fe270c35deb5dfdb174f72e465
Olga-Olga <olga_l@ukr.net>
2023-06-27 16:35:14 +0300

    1234

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// console.log(galleryItems);
+// 
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// console.log(galleryItems);
+
+// const ulElement = document.querySelector(".gallery")
+
+
+
+// ulElement.insertAdjacentHTML(afterbegin, instance)
+    
+    
+    
+    
+const list = document.querySelector(".gallery");
+
+const newTechnologies = ["React", "TypeScript", "Node.js"];
+const markup = newTechnologies
+  .map((technology) => `<li class="list-item new">${technology}</li>`)
+  .join("");
+
+  list.insertAdjacentHTML("beforeend", markup);
+//list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
+
+
+
+
+//instance.show()
+
+
+
